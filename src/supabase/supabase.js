9fa20d33c44a6e4a05528820a10e9d177f4f772f
Rayen <51766016+rrayeen@users.noreply.github.com>
2024-10-17 09:03:18 +0100

    Update supabase.js

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -2,9 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import {createClient} from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://mmhehytxuczxkcfinnly.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1taGVoeXR4dWN6eGtjZmlubmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU3ODA0OTcsImV4cCI6MjAzMTM1NjQ5N30.r02QqjgTy60HXgwV1hGEy_7anZsMm2XmvaJyqrCB3YU';
+const supabaseUrl = 'SUPABASE_URL';
+const supabaseAnonKey = 'SUPABASE_KEY';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
